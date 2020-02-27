0d85e649bde7568ac35d49e9a81dd650e8b3ab5f
richardljohn <richardljohn@gmail.com>
2020-02-26 21:04:38 -0500

    Structure of Player

---
+++ Player.cpp
@@ -0,0 +1,17 @@
+//This is the class Player. 
+//This is your avatar. 
+
+#include "Player.hpp"
+
+Player::Player(){
+
+}
+
+Player::Player(string name_){
+    setName(name_);
+    name = name_;
+}
+
+void Player::setName(string name_){
+    name = name_;
+}
---
+++ Player.hpp
@@ -0,0 +1,18 @@
+//This is the class player. 
+//He or she is your avatar for your game. 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Player {
+    public:
+        Player();
+        Player(string name_);
+        void setName(string name_);
+        int setBet(int bet);
+
+    private:
+        double money = 100.0;
+        string name;
+};
