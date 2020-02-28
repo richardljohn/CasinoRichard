f241e3a78fab213506eba122b4cb8c1a78838fcf
richardljohn <richardljohn@gmail.com>
2020-02-28 13:19:02 -0500

    Craps Game Stuff

--- CrapsGame.cpp
+++ CrapsGame.cpp
@@ -27,6 +27,60 @@ int Craps::diceRoll(){
 }
 
 int Craps::playCraps(){
-    
-
+    setBet();
+    srand(time(0));
+    while (repeat == 'Y') {
+        roll = diceRoll();
+        if ((roll == 7) || (roll == 11)) {
+            cout << "You win! :)" << endl;
+            count++;
+            wins++;
+            cout << "Another game?! Y(es) or N(o)" << endl;                
+            cin >> repeat;
+            setBet();
+        }
+        else if ((roll == 2) || (roll == 3) || (roll == 12)) {
+            cout << "You lose... :(" << endl;
+            count++;
+            losses++;
+            cout << "Another game??.... Y(es) or N(o)" << endl;
+            cin >> repeat;
+            if(repeat == 'Y'){
+                setBet();
+            }
+        }
+        else if ((roll == 4) || (roll == 5) || (roll == 6) || (roll == 8) || (roll == 9) || (roll == 10)) {
+            point = roll;
+            cout << point << endl;
+            d1 = rand() % 6 + 1;
+            d2 = rand() % 6 + 1;
+            newRoll = d1 + d2;
+            cout << newRoll << endl;
+            if (newRoll == point) {
+                cout << "You win! :)" << endl;
+                count++;
+                wins++;
+                cout << "Another game?! Y(es) or N(o)" << endl;
+                cin >> repeat;
+                if(repeat == 'Y'){
+                    setBet();
+                }
+            }
+            else if (newRoll == 7) {
+                cout << "You lose... :(" << endl;
+                count++;
+                losses++;
+                cout << "Another game??... Y(es) or N(o)" << endl;
+                cin >> repeat;
+                if(repeat == 'Y'){
+                    setBet();
+                }
+            }
+            else {
+                d1 = rand() % 6 + 1;
+                d2 = rand() % 6 + 1;
+                roll = d1 + d2;
+            }
+        }
+    }
 }
